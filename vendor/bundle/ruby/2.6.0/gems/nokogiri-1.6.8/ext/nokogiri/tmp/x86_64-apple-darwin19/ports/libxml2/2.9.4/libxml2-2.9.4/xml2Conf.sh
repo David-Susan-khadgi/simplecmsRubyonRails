a4def215582de6e42643840aa0e1986508b50a81
Susan khadgi <susankhadgi20@gmail.com>
2020-10-29 09:53:39 +1100

    First Commit

---
+++ vendor/bundle/ruby/2.6.0/gems/nokogiri-1.6.8/ext/nokogiri/tmp/x86_64-apple-darwin19/ports/libxml2/2.9.4/libxml2-2.9.4/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/susankhadgi/Files/LinkedIn/Become a full stack developer/Ruby on Rail 5 Essential Training/Ex_Files_RubyonRails5_EssT/Chapter_15/15_08-final/simple_cms/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.6.8/ports/x86_64-apple-darwin19/libxml2/2.9.4/lib"
+XML2_LIBS="-lxml2 -lz -llzma -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/susankhadgi/Files/LinkedIn/Become a full stack developer/Ruby on Rail 5 Essential Training/Ex_Files_RubyonRails5_EssT/Chapter_15/15_08-final/simple_cms/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.6.8/ports/x86_64-apple-darwin19/libxml2/2.9.4/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
